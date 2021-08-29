831d9b6e9dc297932b62c538ec730d9384653161
Roman <47541355+RLobodenko@users.noreply.github.com>
2021-08-29 19:23:33 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,22 +5,29 @@ var man= {
 };
 
 
+
+
 function f1() {
 var out="";
+    var outt="";
 man.ФИО = document.forms["form1"]['name'].value;
    
 for (key in man){
   out += man[key] ;
- 
+
 }
 
-localStorage.setItem("out1",out);
+    
+
 
     
-    
+localStorage.setItem("out1",out);
+
+
 
 }
 
 
 var out = localStorage.getItem('out1');
 document.getElementById('out').innerHTML = out;
+
